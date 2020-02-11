f1729ea223752e674af68465db5955e09834841a
Yerowyn <yerowyn@gmail.com>
2020-02-11 23:14:51 +0600

    passport config

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const next = require('next');
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -16,21 +19,64 @@ const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async function(email, password, done) {
+      if (!email || !password) {
+        done('Email and password required', null);
+        return;
+      }
+
+      const user = await User.findOne({ where: { email } });
+
+      if (!user) {
+        done('User not found', null);
+        return;
+      }
+
+      const valid = await user.isPasswordValid(password);
+
+      if (!valid) {
+        done('Email and password do not match', null);
+        return;
+      }
+
+      done(null, user);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.email);
+});
+
+passport.deserializeUser((email, done) => {
+  User.findOne({ where: { email } }).then(user => {
+    done(null, user);
+  });
+});
+
 nextApp.prepare().then(() => {
   const server = express();
 
   server.use(
     session({
-      secret: '343ji43j4n3jn4jk3n',
+      secret: '343ji43j4n3jn4jk3n', // enter a random string here
       resave: false,
       saveUninitialized: true,
       name: 'nextbnb',
       cookie: {
-        secure: false,
-        maxAge: 30 * 24 * 60 * 60 * 1000,
+        secure: false, // CRITICAL on localhost
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       },
       store: sessionStore,
-    })
+    }),
+    passport.initialize(),
+    passport.session()
   );
 
   sessionStore.sync();
