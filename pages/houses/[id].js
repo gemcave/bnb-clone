5c8305d3bccaf17019deeb70570fe657713501a3
Yerowyn <yerowyn@gmail.com>
2020-02-11 08:17:46 +0600

    DateRangePicker component

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import houses from '../houses.json';
 import Layout from '../../components/Layout.js';
+import DateRangePicker from '../../components/DateRangePicker';
 
 const House = props => (
   <Layout
@@ -21,6 +22,7 @@ const House = props => (
         </article>
         <aside>
           <h2>Add dates for prices</h2>
+          <DateRangePicker />
         </aside>
         <style jsx>{`
           .container {
