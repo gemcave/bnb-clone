96d5bdee54d38b6e95db1d81287f6af37e608f62
Yerowyn <yerowyn@gmail.com>
2020-02-12 19:58:35 +0600

    login from modal

--- server.js
+++ server.js
@@ -132,6 +132,52 @@ nextApp.prepare().then(() => {
     }
   });
 
+  server.post('/api/auth/login', async (req, res) => {
+    passport.authenticate('local', (err, user, info) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end(
+          JSON.stringify({
+            status: 'error',
+            message: err,
+          })
+        );
+        return;
+      }
+
+      if (!user) {
+        res.statusCode = 500;
+        res.end(
+          JSON.stringify({
+            status: 'error',
+            message: 'No user matching credentials',
+          })
+        );
+        return;
+      }
+
+      req.login(user, err => {
+        if (err) {
+          res.statusCode = 500;
+          res.end(
+            JSON.stringify({
+              status: 'error',
+              message: err,
+            })
+          );
+          return;
+        }
+
+        return res.end(
+          JSON.stringify({
+            status: 'success',
+            message: 'Logged in',
+          })
+        );
+      });
+    })(req, res, next);
+  });
+
   server.all('*', (req, res) => handle(req, res));
 
   server.listen(port, err => {
