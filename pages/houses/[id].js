0271a7e0ae843cabfad04c7ce4ae6d7b11c9f519
Yerowyn <yerowyn@gmail.com>
2020-02-12 22:49:33 +0600

    connect view to db

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { useStoreActions } from 'easy-peasy';
+import fetch from 'isomorphic-unfetch';
 
 import Head from 'next/head';
-import houses from '../houses.json';
 import Layout from '../../components/Layout.js';
 import DateRangePicker from '../../components/DateRangePicker';
 
@@ -41,9 +41,20 @@ const House = props => {
               {props.house.type} - {props.house.town}
             </p>
             <p>{props.house.title}</p>
-            <p>
-              {props.house.rating} ({props.house.reviewsCount})
-            </p>
+            {props.house.reviewsCount ? (
+              <div className="reviews">
+                <h3>{props.house.reviewsCount} Reviews</h3>
+
+                {props.house.reviews.map((review, index) => (
+                  <div key={index}>
+                    <p>{new Date(review.createdAt).toDateString()}</p>
+                    <p>{review.comment}</p>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <></>
+            )}
           </article>
           <aside>
             <h2>Add dates for prices</h2>
@@ -91,11 +102,14 @@ const House = props => {
     />
   );
 };
-House.getInitialProps = ({ query }) => {
+House.getInitialProps = async ({ query }) => {
   const { id } = query;
 
+  const res = await fetch(`http://localhost:3000/api/houses/${id}`);
+  const house = await res.json();
+
   return {
-    house: houses.filter(house => house.id === id)[0],
+    house,
   };
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,39 @@
-import houses from './houses.json';
-import House from '../components/House';
-import Layout from '../components/Layout.js';
+import fetch from 'isomorphic-unfetch';
 
-const Index = () => {
-  const content = (
-    <div>
-      <h2>Places to stay</h2>
-      <div className="houses">
-        {houses.map((house, index) => (
-          <House key={index} {...house} />
-        ))}
+import House from '../components/House';
+import Layout from '../components/Layout';
+
+const Index = props => (
+  <Layout
+    content={
+      <div>
+        <h2>Places to stay</h2>
+
+        <div className="houses">
+          {props.houses.map((house, index) => (
+            <House key={index} {...house} />
+          ))}
+        </div>
+
+        <style jsx>{`
+          .houses {
+            display: grid;
+            grid-template-columns: 50% 50%;
+            grid-template-rows: 300px 300px;
+            grid-gap: 40px;
+          }
+        `}</style>
       </div>
-      <style jsx>{`
-        .houses {
-          display: grid;
-          grid-template-columns: 50% 50%;
-          grid-template-rows: 300px 300px;
-          grid-gap: 40px;
-        }
-      `}</style>
-    </div>
-  );
-  return <Layout content={content} />;
+    }
+  />
+);
+
+Index.getInitialProps = async () => {
+  const res = await fetch(`http://localhost:3000/api/houses`);
+  const houses = await res.json();
+  return {
+    houses,
+  };
 };
 
 export default Index;
