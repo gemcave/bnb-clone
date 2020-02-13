fb5ee6e532c8ec3005804e266c97ea447f877da3
Yerowyn <yerowyn@gmail.com>
2020-02-14 00:34:31 +0600

    houses and bookings page

--- components/Header.js
+++ components/Header.js
@@ -31,6 +31,11 @@ const Header = () => {
                 </Link>
               </li>
               <li>
+                <Link href="/host">
+                  <a>Your Houses</a>
+                </Link>
+              </li>
+              <li>
                 <a
                   href="#"
                   onClick={async () => {
--- server.js
+++ server.js
@@ -490,6 +490,64 @@ nextApp.prepare().then(() => {
       res.end(JSON.stringify(bookings));
     });
   });
+
+  server.get('/api/host/list', async (req, res) => {
+    if (!req.session.passport || !req.session.passport.user) {
+      res.writeHead(403, {
+        'Content-Type': 'application/json',
+      });
+      res.end(
+        JSON.stringify({
+          status: 'error',
+          message: 'Unauthorized',
+        })
+      );
+
+      return;
+    }
+
+    const userEmail = req.session.passport.user;
+    const user = await User.findOne({ where: { email: userEmail } });
+
+    const houses = await House.findAll({
+      where: {
+        host: user.id,
+      },
+    });
+    const houseIds = houses.map(house => house.dataValues.id);
+
+    const bookingsData = await Booking.findAll({
+      where: {
+        paid: true,
+        houseId: {
+          [Op.in]: houseIds,
+        },
+        endDate: {
+          [Op.gte]: new Date(),
+        },
+      },
+      order: [['startDate', 'ASC']],
+    });
+
+    const bookings = await Promise.all(
+      bookingsData.map(async booking => ({
+        booking: booking.dataValues,
+        house: houses.filter(
+          house => house.dataValues.id === booking.dataValues.houseId
+        )[0].dataValues,
+      }))
+    );
+
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(
+      JSON.stringify({
+        bookings,
+        houses,
+      })
+    );
+  });
   server.all('*', (req, res) => handle(req, res));
 
   server.listen(port, err => {
