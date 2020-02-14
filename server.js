fc47d1418d468692b02bd9ac94259697b3b89526
Yerowyn <yerowyn@gmail.com>
2020-02-14 10:07:39 +0600

    file upload

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const User = require('./models/user.js');
 const House = require('./models/house.js');
 
+const fileupload = require('express-fileupload');
+
+const randomstring = require('randomstring');
+
 const sanitizeHtml = require('sanitize-html');
 
 const dotenv = require('dotenv');
@@ -129,7 +133,8 @@ nextApp.prepare().then(() => {
       store: sessionStore,
     }),
     passport.initialize(),
-    passport.session()
+    passport.session(),
+    fileupload()
   );
 
   server.post('/api/auth/logout', (req, res) => {
@@ -651,6 +656,44 @@ nextApp.prepare().then(() => {
           );
         }
       });
+    });
+  });
+
+  server.post('/api/host/image', (req, res) => {
+    if (!req.session.passport) {
+      res.writeHead(403, {
+        'Content-Type': 'application/json',
+      });
+      res.end(
+        JSON.stringify({
+          status: 'error',
+          message: 'Unauthorized',
+        })
+      );
+
+      return;
+    }
+
+    const { image } = req.files;
+    const fileName = randomstring.generate(7) + image.name.replace(/\s/g, '');
+    const path = `${__dirname}/public/img/houses/${fileName}`;
+
+    image.mv(path, error => {
+      if (error) {
+        console.error(error);
+        res.writeHead(500, {
+          'Content-Type': 'application/json',
+        });
+        res.end(JSON.stringify({ status: 'error', message: error }));
+        return;
+      }
+
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      res.end(
+        JSON.stringify({ status: 'success', path: `/img/houses/${fileName}` })
+      );
     });
   });
 
